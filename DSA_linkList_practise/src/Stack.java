102ad87986582f75b85c95392725d4785b134eed
sarim nadeem <115877996+samcodeonline@users.noreply.github.com>
2024-04-18 16:26:19 +0500

    update

--- DSA_linkList_practise/src/Stack.java
+++ DSA_linkList_practise/src/Stack.java
@@ -1,6 +1,4 @@
 public class Stack {
-//    as the element would be pushed the element would go to the place of the array or array list and then
-//    the stack would be underflow.
     public static class stack_str {
     int[] nodeValue = new int[5];
     stack_str nextNodeReferencePointer;
